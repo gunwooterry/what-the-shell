fdb87e160d9f361248c72059ef1864276d6efbd7
Gunwoo Kim <gunwooterry@gmail.com>
2017-05-06 20:02:12 +0900

    Reformatted to follow the Airbnb style guide

--- main.js
+++ main.js
@@ -18,14 +18,14 @@ $(document).ready(() => {
           {
             type: 'file',
             name: 'hello.c',
-            path: 'root/aaa/hello.c'
+            path: 'root/aaa/hello.c',
           },
           {
             type: 'file',
             name: 'hi.txt',
-            path: 'root/aaa/hi.txt'
-          }
-        ]
+            path: 'root/aaa/hi.txt',
+          },
+        ],
       },
       {
         type: 'folder',
@@ -35,55 +35,55 @@ $(document).ready(() => {
           {
             type: 'file',
             name: 'trash.txt',
-            path: 'root/bbb/trash.txt'
-          }
-        ]
+            path: 'root/bbb/trash.txt',
+          },
+        ],
       },
       {
         type: 'file',
         name: 'README.md',
         path: 'root/README.md',
-        children: []
-      }
-    ]
-  }
-  let current = root
-  let sidebar = document.getElementById('sidebar')
-  let finder = document.getElementById('finder')
-  sidebar.appendChild(renderHierarchy(root))
-  renderFinder(current)
+        children: [],
+      },
+    ],
+  };
+
+  let current = root;
+  const sidebar = document.getElementById('sidebar');
+  const finder = document.getElementById('finder');
+  sidebar.appendChild(renderHierarchy(root));
+  renderFinder(current);
 })
 
 function renderHierarchy(current) {
-  let currentDir = document.createElement('div')
-  currentDir.classList.add('ui', 'accordion')
-  for (let child of current.children) {
-    let title = document.createElement('div')
-    let dropdown = document.createElement('i')
-    let icon = document.createElement('i')
-    let name = document.createTextNode(child.name)
+  const currentDir = document.createElement('div');
+  currentDir.classList.add('ui', 'accordion');
+  current.children.forEach(child => {
+    const { type, name } = child
+    if (type === 'folder') {
+      const title = document.createElement('div');
+      const dropdown = document.createElement('i');
+      const icon = document.createElement('i');
+      const nameText = document.createTextNode(name);
 
-    title.classList.add('active', 'title')
-    dropdown.classList.add('dropdown', 'icon')
-    title.appendChild(dropdown)
-    title.appendChild(icon)
-    title.appendChild(name)
-    currentDir.appendChild(title)
+      title.classList.add('active', 'title');
+      dropdown.classList.add('dropdown', 'icon');
+      icon.classList.add('folder', 'icon');
+      title.appendChild(dropdown);
+      title.appendChild(icon);
+      title.appendChild(nameText);
+      currentDir.appendChild(title);
 
-    if (child.type != 'folder') dropdown.style.opacity = 0
-    if (child.type == 'folder') icon.classList.add('folder', 'icon')
-    else if (child.type == 'file') icon.classList.add('file', 'icon')
-
-    if (child.type == 'folder') {
-      let content = document.createElement('div')
-      content.classList.add('active', 'content')
-      content.style.marginTop = '-1em'
-      content.style.padding = '0 0 0 1em'
-      content.appendChild(renderHierarchy(child))
-      currentDir.appendChild(content)
+      const content = document.createElement('div');
+      content.classList.add('active', 'content');
+      content.style.marginTop = '-1em';
+      content.style.padding = '0 0 0 1em';
+      content.appendChild(renderHierarchy(child));
+      currentDir.appendChild(content);
     }
-  }
-  return currentDir
+  });
+
+  return currentDir;
 }
 
 function renderFinder(current) {
