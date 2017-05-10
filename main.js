072d1a0ba07c17028d61904b8d86e479a08ed6cd
Zeniuus <jsh56son@kaist.ac.kr>
2017-05-11 05:28:50 +0900

    manual skeleton code

--- main.js
+++ main.js
@@ -477,9 +477,16 @@ function showManual(command) {
   const manualModal = document.getElementById('manual');
   const header = document.getElementById('manual_header');
   const description = document.getElementById('manual_desc');
-  const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  let loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
   header.innerHTML = command;
+  switch (command.split(' ')[0]) {
+    case 'cd' : loremIpsum = 'cd means changing directory'; break;
+    case 'cp' : loremIpsum = 'cp means copy'; break;
+    case 'cat' : loremIpsum = 'cat shows the target file\'s content'; break;
+    case 'rm' : loremIpsum = 'rm means remove'; break;
+    default : break;
+  }
   description.innerHTML = loremIpsum;
   manualModal.style.visibility = 'visible';
 }
