b75882ae221365eca0f7b3e74ce9b1325bb4dc8b
Youngkyu <yk.hong@kaist.ac.kr>
2017-05-25 20:37:05 +0900

    manual changed

--- description.js
+++ description.js
@@ -16,13 +16,13 @@ var manuals = {
     how to use : rm [1. flags(optional)] [2. files and directories to remove] <br>
 <br>
     1. flags : You can add flags to do special things. <br>
-    > ‘-r’ (recursive) : If you want to remove folders, you should add this flag. <br>
-    > ‘-f’ (force) : If you want to force to remove action, you can do that by add this flag. <br>
-    If you want to force to remove folders, than you can add ‘-rf’ flag. <br>
+    > '-r' (recursive) : If you want to remove folders, you should add this flag. <br>
+    > '-f' (force) : If you want to force to remove action, you can do that by add this flag. <br>
+    If you want to force to remove folders, than you can add '-rf' flag. <br>
     2. files and directories to remove : You can remove files or directories as much as you want. You should separate each files and directories by space. They can be not in the current directory, but in this case you should indicate their path. <br>
 <br>
     e.g. rm -r hello.txt hi.txt ccc bbb => remove hello.txt, hi.txt, and ccc folder to bbb folder. <br>
-    e.g. rm hello.txt hi.txt ccc bbb => remove hello.txt and hi.txt to bbb folder, but failed to remove ccc folder because there is no ‘-r’ flag. <br>
+    e.g. rm hello.txt hi.txt ccc bbb => remove hello.txt and hi.txt to bbb folder, but failed to remove ccc folder because there is no '-r' flag. <br>
     e.g. rm -f hi.txt => force to remove hi.txt <br>
     e.g. rm -rf aaa/bbb => force to remove bbb folder in aaa folder.<br>`,
 
@@ -31,9 +31,9 @@ var manuals = {
     how to use : cd [1. target directory]<br>
 <br>
     1. target directory : Target directory can be either absolute path(path starting with ~/ or /) or relative path to the current directory.<br>
-    There are special keywords ‘.’ and ‘..’, which means :<br>
-    > ‘.’ : current directory<br>
-    > ‘..’ : parent directory of current directory<br>
+    There are special keywords . and .., which means :<br>
+    > . : current directory<br>
+    > .. : parent directory of current directory<br>
     If you want to jump into directories in depth, you should use its path. <br>
 <br>
     e.g. cd .. => move to the parent directory of the current directory. <br>
@@ -48,8 +48,8 @@ var manuals = {
 <br>
     1. directory name to make : You should write a directory name that does not exist. mkdir basically makes a new directory with given name in the current directory, but you can make a new directory in other than the current directory by using '/' between each directory. In this case, the directory path should exist. <br>
 <br>
-    e.g. mkdir aaa => make a new directory of name ‘aaa’ in the current directory. If aaa folder already exists in the current directory, it failes. <br>
-    e.g. mkdir bbb/aaa => make a new directory of name ‘aaa’ in bbb folder.<br>`,
+    e.g. mkdir aaa => make a new directory of name 'aaa' in the current directory. If aaa folder already exists in the current directory, it failes. <br>
+    e.g. mkdir bbb/aaa => make a new directory of name 'aaa' in bbb folder.<br>`,
 
     mv: `mv - move files or directories to other directory or rename them.<br>
 <br>
@@ -84,10 +84,10 @@ var manuals = {
     how to use : cp [1. flags(optional)] [2. files and directories to copy] [3. target directory]<br>
 <br>
     1. flags : You can add flags to do special things.<br>
-    > ‘-r’ (recursive) : If you want to copy folders, you should add this flag.<br>
+    > '-r' (recursive) : If you want to copy folders, you should add this flag.<br>
     2. files and directories to copy : You can copy files or directories as much as you want. You should separate each files and directories by space.<br>
     3. target directory : Target directory should not be the current directory. It can be either absolute path(path starting with ~/ or /) or relative path to the current directory.<br>
 <br>
     e.g. cp -r hello.txt hi.txt ccc bbb => copy hello.txt, hi.txt, and ccc folder to bbb folder.<br>
-    e.g. cp hello.txt hi.txt ccc bbb => copy hello.txt and hi.txt to bbb folder, but failed to copy ccc folder because there is no ‘-r’ flag. <br>`,
+    e.g. cp hello.txt hi.txt ccc bbb => copy hello.txt and hi.txt to bbb folder, but failed to copy ccc folder because there is no '-r' flag. <br>`,
   }
