6d3edf58514d4a54c978d65460577310f73d2b95
Gunwoo Kim <gunwooterry@gmail.com>
2017-11-22 12:51:21 +0900

    fix(breadcrumb): enable changing directory by clicking breadcrumb

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -9,21 +9,36 @@ interface BreadcrumbProps {
 
 class FinderBreadcrumb extends React.Component<BreadcrumbProps> {
   render() {
-    const pathSplit = this.props.currentDirectory.getPath().split('/');
+    const currentDirectory = this.props.currentDirectory;
+    const lineage: Folder[] = [currentDirectory];
+    let folderWalker = currentDirectory.parent;
+    while (folderWalker) {
+      lineage.push(folderWalker);
+      folderWalker = folderWalker.parent;
+    }
+    lineage.reverse();
+
+    const activeSection = (directory: Folder) =>
+      <Breadcrumb.Section active={true}>{directory.name}</Breadcrumb.Section>;
+    const inactiveSection = (directory: Folder) => (
+      <span>
+        <Breadcrumb.Section
+          link={true}
+          onClick={() => this.props.directoryHandler(directory)}
+        >
+          {directory.name}
+        </Breadcrumb.Section>
+        <Breadcrumb.Divider />
+      </span>
+    );
+
     return (
       <Segment basic={true} style={{ margin: 0, height: 36 }}>
         <Breadcrumb>
           {
-            pathSplit.map(
-              (directoryName, idx, array) => (
-                idx === array.length - 1 ?
-                  <Breadcrumb.Section active={true}>{directoryName}</Breadcrumb.Section> :
-                  (
-                    <span>
-                      <Breadcrumb.Section link={true}>{directoryName}</Breadcrumb.Section>
-                      <Breadcrumb.Divider />
-                    </span>
-                  )
+            lineage.map(
+              (directory, idx, array) => (
+                idx === array.length - 1 ? activeSection(directory) : inactiveSection(directory)
               )
             )
           }
